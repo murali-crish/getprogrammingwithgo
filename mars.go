eb30495e9a318cfaf07277f09a017b3c110fe5a0
murali-crish <murali.crish@gmail.com>
2019-09-19 23:45:40 +1000

    Add listing 2.1

---
+++ mars.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("My weight on the surface of Mars is ")
+	fmt.Print(72.5 * 0.3783)
+	fmt.Print(" Kgs, and I would be ")
+	fmt.Print(37 * 365 / 687)
+	fmt.Print(" years old")
+}
