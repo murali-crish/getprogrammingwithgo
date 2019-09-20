8b3dfcff04043beb36d2cb92a709fcedf5692f5c
murali-crish <murali.crish@gmail.com>
2019-09-20 12:35:52 +1000

    Add listing 2.3

--- mars.go
+++ mars.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Printf("My weight on the surface of %v is %v\n", "Earth", 72.5)
 	fmt.Printf("My weight on the surface of Mars is %v Kgs,", 72.5*0.3783)
 	fmt.Printf(" and I would be %v years old.\n", 37*365/687)
 }
